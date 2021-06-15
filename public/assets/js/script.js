b6957b10952fb6990e1ddc0ffa4e27498967880c
Don-SKRED <ruddynomenjanahary@gmail.com>
2021-06-15 09:59:32 +0300

    update du 15/06/2021

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -43,23 +43,24 @@ $(document).ready(function($) {
         },
 
     });
-     $('.table_data3').DataTable({
+     $('#data_table').DataTable({
 
-        "language": {
-            "lengthMenu": "Afficher _MENU_ données par pages",
-            "zeroRecords": "Nous n'avons rien trouvé - désolé",
-            "info": " page _PAGE_ sur _PAGES_",
-            "infoEmpty": "Aucun enregistrement disponibles",
-            "infoFiltered": "(Filtrer à partir de _MAX_ enregistrement)",
-            "search": "Rechercher:",
-            "paginate": {
-                "first": "Premier",
-                "last": "Dernier",
-                "next": "Suivant",
-                "previous": "Précedent"
+            "language": {
+                "lengthMenu": "Afficher _MENU_ données par pages",
+                "zeroRecords": "Nous n'avons rien trouvé - désolé",
+                "info": " page _PAGE_ sur _PAGES_",
+                "infoEmpty": "Aucun enregistrement disponibles",
+                "infoFiltered": "(Filtrer à partir de _MAX_ enregistrement)",
+                "search": "Rechercher:",
+                "paginate": {
+                    "first": "Premier",
+                    "last": "Dernier",
+                    "next": "Suivant",
+                    "previous": "Précedent"
+                },
             },
-        },
 
-    });
+        });
+ 
 });
 
